3c2ee0a4598ec93da69f30b6e0b4ec0e1f2d97e8
Giulio Cesare Solaroli <giulio.cesare@solaroli.it>
2017-09-07 00:06:23 +0200

    Code cleanup

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -15,41 +15,62 @@ filterObjects = function (anObject) {
     }
 }
 
-deltat = function (xyz1, xyz0, dt) {
+deltaT = function (x1, x0, dt) {
+    return (x1 - x0) / dt;
+}
+
+xyzDeltaT = function (xyz1, xyz0, dt) {
     return {
-        x: (xyz1['x'] - xyz0['x']) / dt,
-        y: (xyz1['y'] - xyz0['y']) / dt,
-        z: (xyz1['z'] - xyz0['z']) / dt
+        x: deltaT(xyz1['x'], xyz0['x'], dt),
+        y: deltaT(xyz1['y'], xyz0['y'], dt),
+        z: deltaT(xyz1['z'], xyz0['z'], dt)
     }
 }
+
+//=================================================================
 
 let namespace = '/dataLogger';
 let socket = io.connect('http://' + document.domain + ':' + location.port + namespace);
 let rawEventsRx = RxfromIO(socket, 'broadcast');
-let eventsRx = rawEventsRx.map(function (e) { return {object:e['object'], timestamp:(new Date(e['timestamp']).getTime()), position:{ x:e['x'], y:e['y'], z:e['z']}}; });
-eventsRx.subscribe(function(x) { console.log("broadcast", x); });
+let eventsRx = rawEventsRx.map(function (e) {
+    return {
+        object:e['object'],
+        timestamp:(new Date(e['timestamp']).getTime()),
+        position: {
+            x:e['x'],
+            y:e['y'],
+            z:e['z']
+        }
+    };
+});
+
+//.................................................................
 
 let objects = ["A", "B", "C"];
+
 let positionsRx = objects.map(x => eventsRx.filter(filterObjects(x)));
+
 let velocitiesRx = positionsRx.map(p => p.bufferCount(2, 1).map(function ([p0, p1]) {
     let dt = p1['timestamp'] - p0['timestamp'];
     return {
         object:    p1['object'],
         timestamp: p1['timestamp'],
         position:  p1['position'],
-        speed:     deltat(p1['position'], p0['position'], dt)
+        speed:     xyzDeltaT(p1['position'], p0['position'], dt)
     }
 }));
+
 let accellerationsRx = velocitiesRx.map(p => p.bufferCount(2, 1).map(function ([v0, v1]) {
     let dt = v1['timestamp'] - v0['timestamp'];
     return {
         object:        v1['object'],
         timestamp:     v1['timestamp'],
         position:      v1['position'],
-        accelleration: deltat(v1['speed'], v0['speed'], dt)
+        accelleration: xyzDeltaT(v1['speed'], v0['speed'], dt)
     }
 }));
 
+eventsRx.subscribe(function(x) { console.log("broadcast", x); });
 positionsRx.map(p => p.subscribe(x => console.log("position " + x['object'], x)));
 velocitiesRx.map(v => v.subscribe(x => console.log("velocity " + x['object'], x['speed'])));
 accellerationsRx.map(a => a.subscribe(x => console.log("accelleration " + x['object'], x['accelleration'])));
